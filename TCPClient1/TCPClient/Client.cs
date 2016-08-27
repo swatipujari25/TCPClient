f74e6736ecd0a30856249be862e0ffaf4250915f
swati <swatipujari25@gmail.com>
2016-08-27 17:30:45 +0530

    Initial

---
+++ TCPClient1/TCPClient/Client.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPClient
+{
+    public partial class Client : Form
+    {       
+
+        public Client()
+        {
+            InitializeComponent();
+        }
+        public delegate void AppendDataDelegate();
+        AppendDataDelegate dataDelegate = null;
+        string recvMsg = string.Empty;
+
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
+
+
+        private void BindDataToTextBox()
+        {
+            rtxDisplay.AppendText(readData);
+        }
+
+        private void InvokeTextBox()
+        {
+            rtxDisplay.Invoke(dataDelegate);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //byte[] outStream = System.Text.Encoding.ASCII.GetBytes(txtUser.Text+ "$");
+            //serverStream.Write(outStream, 0, outStream.Length);
+            //serverStream.Flush();
+
+            readData = "Conected to Chat Server ...";
+            msg();
+
+            if (!clientSocket.Connected)
+            {
+                clientSocket.Connect("127.0.0.1", 8000);
+            }
+            serverStream = clientSocket.GetStream();
+
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(txtUser.Text );
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            Thread ctThread = new Thread(getMessage);
+            ctThread.Start();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {           
+
+            if (!clientSocket.Connected)
+            {
+                clientSocket.Connect("127.0.0.1", 8000);
+            }
+            serverStream = clientSocket.GetStream();
+
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(txtData.Text + "\r\n");
+            serverStream.Write(outStream, 0, outStream.Length);
+            serverStream.Flush();
+
+            
+        }
+
+        private void getMessage()
+        {
+            while (true)
+            {
+                serverStream = clientSocket.GetStream();
+                int buffSize = 0;
+                byte[] inStream = new byte[10025];
+                buffSize = clientSocket.ReceiveBufferSize;
+                serverStream.Read(inStream, 0, buffSize);
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                readData = "" + returndata;
+                msg();
+            }
+        }
+
+        private void msg()
+        {
+            //if (this.InvokeRequired)
+            //    this.Invoke(new MethodInvoker(msg));
+            //else
+            //    //rtxDisplay.Text = textBox1.Text + Environment.NewLine + " >> " + readData;
+            //    // rtxDisplay.AppendText( Environment.NewLine + " >> " + readData);
+            //    recvMsg = Environment.NewLine + " >> " + readData;
+            InvokeTextBox();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataDelegate = new AppendDataDelegate(BindDataToTextBox);
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //timer1.Start();
+            //if (!string.IsNullOrEmpty(recvMsg))
+            //{
+            //    dataDelegate();
+            //    recvMsg = string.Empty;
+            //}
+        } 
+    }
+}
---
+++ TCPClient1/TCPClient/Client.designer.cs
@@ -0,0 +1,118 @@
+﻿namespace TCPClient
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.rtxDisplay = new System.Windows.Forms.RichTextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(444, 385);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(61, 52);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(13, 385);
+            this.txtData.Multiline = true;
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(425, 52);
+            this.txtData.TabIndex = 1;
+            // 
+            // rtxDisplay
+            // 
+            this.rtxDisplay.Location = new System.Drawing.Point(13, 48);
+            this.rtxDisplay.Name = "rtxDisplay";
+            this.rtxDisplay.Size = new System.Drawing.Size(425, 331);
+            this.rtxDisplay.TabIndex = 2;
+            this.rtxDisplay.Text = "";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(444, 48);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(68, 52);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(13, 13);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(193, 20);
+            this.txtUser.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 459);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.rtxDisplay);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.RichTextBox rtxDisplay;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
